934b3297c5e1e17d3361a0d85756f34346987416
amithasan102827e <amithasan102827@gmail.comm>
2021-10-31 02:04:41 +0600

    six commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
   const { user, logOut } = useAuth();
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light bg-$blue-600">
         <div className="container-fluid">
           <a className="navbar-brand text-danger" href="#">travel.com</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
